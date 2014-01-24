f995b4f1b614e73cf48dd508e3965c118045e55f
Justin Edwards <justinledwards@gmail.com>
2014-01-23 21:47:20 -0600

    Missed get on url change

--- lib/such_doge_api.rb
+++ lib/such_doge_api.rb
@@ -16,7 +16,7 @@ module SuchDogeApi
   end
 
   def self.get_payment_address(payment_address_label)
-    endpoint = "new_address&address_label=#{payment_address_label}"
+    endpoint = "get_new_address&address_label=#{payment_address_label}"
     self.api_call(endpoint)
   end
 
